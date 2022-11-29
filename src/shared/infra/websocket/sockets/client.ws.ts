316fc0b6753a03338fe8cd8d96b7c04ad1e4a8f8
Gabriel Frasson <bielfc27@gmail.com>
2022-11-29 17:30:01 -0300

    Client first access webscocket channel

--- src/shared/infra/websocket/sockets/client.ws.ts
+++ src/shared/infra/websocket/sockets/client.ws.ts
@@ -1,8 +1,10 @@
+
+import { CreateConnectionController } from "@modules/connections/useCases/createConnection/CreateConnectionController";
 import { Socket } from "socket.io";
 import { io } from "../io-ws";
 
+const createConnectionController = new CreateConnectionController();
+
 io.on("connect", (socket: Socket) => {
-    socket.on("client_first_access", (params) => {
-        console.log(params);
-    });
+    socket.on("client_first_access", (params) => createConnectionController.handle(socket, params));
 });
